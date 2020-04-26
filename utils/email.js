b856600a8f228e264a5ad62a3577f3df902c79c6
Anuj Sharma <anuj992393@gmail.com>
2020-04-26 13:51:18 +0530

    fixed the email error

--- utils/email.js
+++ utils/email.js
@@ -11,10 +11,6 @@ module.exports = class Email {
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      return 1;
-    }
-
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
